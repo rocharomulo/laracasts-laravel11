5477678d90137ed85a325d5e7459bfc73a338925
Romulo Rocha <romulorj2023@gmail.com>
2024-08-18 15:03:11 -0300

    commit inicial

---
+++ app/Models/Job.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Job extends Model
+{
+    use HasFactory;
+    protected $table = 'job_listings';
+
+    protected $fillable = ['id', 'title', 'salary'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: 'job_listing_id');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Job;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+
+    return view('home');
+});
+
+Route::get('/jobs', function () {
+
+    // $jobs = Job::with('employer')->paginate(4);
+    $jobs = Job::with('employer')->simplePaginate(4);
+
+    return view('jobs', [
+        'jobs' => $jobs
+    ]);
+});
+
+Route::get('/jobs/{id}', function ($id) {
+
+    $job = Job::find($id);
+
+    return view('job', ['job' => $job]);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
