1773a1e91d523393ee413463f2b3942ccfc3a54d
Romulo Rocha <romulorj2023@gmail.com>
2024-08-22 09:52:51 -0300

    aula 24

---
+++ resources/views/mail/job-posted.blade.php
@@ -0,0 +1,8 @@
+<x-mail::message>
+    <b>Nova Vaga: {{ $job->title }}</b>
+
+    Parabéns! Sua vaga de emprego está disponível em nosso site com salário de {{ $job->salary }}.
+
+    Obrigado,
+    Romulo
+</x-mail::message>
--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,25 @@
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
+use App\Mail\JobPosted;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
+
+Route::get('test', function () {
+    // return new JobPosted(); // exibe o corpo do email
+    Mail::to('romulorj2023@gmail.com')->send(new JobPosted);
+
+    return 'Done';
+});
 
 Route::view('/', 'home');
 
 Route::view('contact', 'contact');
 
-Route::resource('jobs', JobController::class)->only(['index', 'show']);
-Route::resource('jobs', JobController::class)->except(['index', 'show'])->middleware('auth');
+// Route::resource('jobs', JobController::class);
+/* Route::resource('jobs', JobController::class)->only(['index', 'show', 'create']);
+Route::resource('jobs', JobController::class)->except(['index', 'show', 'create'])->middleware('auth'); */
+Route::resource('jobs', JobController::class)->middleware('auth');
 
 //Auth
 Route::get('register', [RegisteredUserController::class, 'create']);
